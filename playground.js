56dcaee643ab2ca128cb0d0d5f702364eec8792d
JuanHMelendez <juanh.melendez@gmail.com>
2023-03-23 13:27:13 -0300

    primer ejercicio de readlinesync

---
+++ playground.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var rls = require("readline-sync");
+var nombre = "";
+nombre = rls.question("Ingrese su nombre: ");
+console.log("Usted ingreso", nombre);
