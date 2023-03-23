6bf30b768dff5949a0eedab54207c09be994b698
JuanHMelendez <juanh.melendez@gmail.com>
2023-03-23 14:02:27 -0300

    fix ejercicio1 que faltaba

--- playground.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var rls = require("readline-sync");
-var nombre = "";
-nombre = rls.question("Ingrese su nombre: ");
-console.log("Usted ingreso", nombre);
--- playground.ts
+++
@@ -1,4 +0,0 @@
-import * as rls from "readline-sync";
-let nombre:string = "";
-nombre = rls.question("Ingrese su nombre: ");
-console.log("Usted ingreso", nombre);
