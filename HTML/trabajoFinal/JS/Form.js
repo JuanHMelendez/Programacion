841dd611157f3c22be200f7d239dbcc1b8c3ce81
JuanHMelendez <juanh.melendez@gmail.com>
2023-06-26 22:20:25 -0300

    primer commit

---
+++ HTML/trabajoFinal/JS/Form.js
@@ -0,0 +1,20 @@
+const formulario = document.querySelector ("form");
+formulario.addEventListener ("submit", handlesumit);
+
+
+function handlesumit(event) {
+    event.preventDefault();
+    const user = {
+        nombre: "",
+        apellido: "",
+        observaciones: "",
+
+    }
+    const info = new FormData (formulario);
+    
+    user.nombre= info.get("nombre");
+    user.apellido= info.get("apellido");
+    user.observaciones= info.get("observaciones");
+
+    console.log(user);
+}
